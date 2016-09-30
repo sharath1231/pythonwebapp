8ed75345171fb56150a59d95d535f705e7c881ad
Sharath <sharath@Sharaths-MacBook-Pro.local>
2016-09-29 23:22:36 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from redis import Redis
+
+app = Flask(__name__)
+redis = Redis(host='10.92.2.4', port=6379)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def counter(path):
+  if not redis.exists(path):
+    redis.set(path, 1)
+    redis.expire(path, 300)
+  else:
+    redis.incr(path)
+  
+  return redis.get(path)
+
+if __name__ ==  "__main__":
+  app.run(host="0.0.0.0", port=3000, debug=True)
